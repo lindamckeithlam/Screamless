36a81c2276526e6d352db066dcc36cff5cc90b5d
Linda Mckeith Lam <lindamckeithlam@Lindas-MacBook-Pro.local>
2019-03-26 17:22:52 -0400

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,38 +1,37 @@
 class User < ApplicationRecord
   validates :username, :last_name, :first_name, :password_digest, :session_token, presence: true
   validates :session_token, :username, uniqueness: true
-  validates :password, length: {minimum: 6, allow_nil: true }
+  validates :password, length: {minimum: 6, allow_nil: true}
 
   after_initialize :ensure_session_token
-  attr_reader: :password
+  attr_reader :password
 
-  def password=(password) 
+  def password=(password)
     self.password_digest = BCrypt.create(password)
     @password = password
-  end 
+  end
 
   def is_password?(password)
     BCrypt.new(self.password_digest).is_password?(password)
-  end 
+  end
 
-  def ensure_session_token 
+  def ensure_session_token
     self.session_token ||= self.class.generate_session_token
-  end 
+  end
 
-  
-  def reset_session_token 
+  def reset_session_token
     self.update(self.session_token = self.class.generate_session_token)
     return self.session_token
-  end 
-  
+  end
 
-  private 
+  private
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user && user.is_password?(password)
-  end 
-  
-  def self.generate_session_token 
+  end
+
+  def self.generate_session_token
     SecureRandom::urlsafe_base64
-  end 
+  end
 end
